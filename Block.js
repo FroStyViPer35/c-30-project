e259601cfeec6fedb07e730940cc0dcd58247bbd
FroStyViPer35 <83050310+FroStyViPer35@users.noreply.github.com>
2021-07-13 11:00:04 -0700

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,26 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.Visabliy = 255
+      this.image = loadImage("block.png")
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos= this.body.position;
+      if(this.body.speed <3){
+        super.display();
+      }
+      else{
+        World.remove(world, this.body)
+        this.Visabil = this.Visabliy
+        imageMode(CENTER);
+        image(this.image, pos.x,pos.y,this.width, this.height);
+      }
+    }
+}
